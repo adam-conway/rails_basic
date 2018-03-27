a95ae5a7c37f041f547be018b640cc97c80534ee
adam-conway <adam.n.conway@gmail.com>
2018-03-27 14:22:04 -0600

    Added test for student address creation

---
+++ spec/features/addresses/user_can_create_a_new_address_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'User' do
+  scenario 'User visits /students/new and creates a student' do
+    student = Student.create!(name: "Adam")
+    visit new_student_address_path(student)
+
+    fill_in "address[description]", with: "1123"
+    fill_in "address[street]", with: "Main St"
+    fill_in "address[city]", with: "Denver"
+    fill_in "address[state]", with: "CO"
+    fill_in "address[zip]", with: "80220"
+    click_on "Submit"
+
+    expect(current_path).to eq(student_path(student))
+    expect(page).to have_content(Address.last.description)
+    expect(page).to have_content(Address.last.street)
+    expect(page).to have_content(Address.last.city)
+    expect(page).to have_content(Address.last.state)
+    expect(page).to have_content(Address.last.zip)
+    expect(student.addresses.count).to eq(1)
+  end
+end
