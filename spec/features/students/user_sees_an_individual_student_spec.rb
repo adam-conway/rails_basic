de4f9a911b2bcbce82aecbaf67d810272729d5c3
adam-conway <adam.n.conway@gmail.com>
2018-03-27 14:51:17 -0600

    Added test for ability to see multiple addresses on student show page

--- spec/features/students/user_sees_an_individual_student_spec.rb
+++ spec/features/students/user_sees_an_individual_student_spec.rb
@@ -8,4 +8,19 @@ describe 'User' do
 
     expect(page).to have_content("Adam")
   end
+
+  scenario 'User visits /students/:id and sees many addresses' do
+    student = Student.create!(name: "Adam")
+    address1 = student.addresses.create!(description: "Great", street: "Main", city: "Denver", state: "CO", zip: "80220")
+    address2 = student.addresses.create!(description: "Not good", street: "MLK", city: "SF", state: "CA", zip: "1234")
+    address3 = student.addresses.create!(description: "bad", street: "Champa", city: "NYC", state: "TX", zip: "2456")
+    address4 = student.addresses.create!(description: "opk", street: "Larimer", city: "Houston", state: "NY", zip: "4564")
+
+    visit student_path(student)
+
+    expect(page).to have_content("Great")
+    expect(page).to have_content("MLK")
+    expect(page).to have_content("NYC")
+    expect(page).to have_content("4564")
+  end
 end
