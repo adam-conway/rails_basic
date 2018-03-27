f9f290bf5dbc112d85d7975eea39b42357d8e44b
adam-conway <adam.n.conway@gmail.com>
2018-03-27 14:16:27 -0600

    Add links from index page to go to student show page

--- spec/features/students/user_sees_all_students_spec.rb
+++ spec/features/students/user_sees_all_students_spec.rb
@@ -17,4 +17,18 @@ describe 'User' do
     expect(page).to have_content(student5.name)
     expect(Student.all.count).to eq(5)
   end
+
+  scenario 'User visits /students and goes to student show page' do
+    student1 = Student.create!(name: "Adam")
+    student2 = Student.create!(name: "Evan")
+    student3 = Student.create!(name: "Kelly")
+    student4 = Student.create!(name: "Jake")
+    student5 = Student.create!(name: "Kolya")
+
+    visit students_path
+    click_link "Kelly"
+
+    expect(page).to have_content(student3.name)
+    expect(current_path).to eq(student_path(student3))
+  end
 end
